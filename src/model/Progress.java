55158e8c9d17d29a7dae17dd4605984eb6ab1b01
Sasaran Andrei Paul <sasaranandreipaul@gmail.com>
2021-02-02 12:35:40 +0200

    clasa progres functioneaza

--- src/model/Progress.java
+++ src/model/Progress.java
@@ -1,9 +1,65 @@
 package model;
-
+/*
+    Clasa care indica progresul in cadrul unui Goal / Task.
+    Valoare procentului dupa nivele:
+    0 - Just Started
+    1 - Something Done
+    2 - Working
+    3 - Almost Done
+    4 - Done
+    Valoarea progresului in procente:
+    [0 - 20)    - nivel 0 - Just Started
+    [20 - 40)   - nivel 1 - Something Done
+    [40 - 70)   - nivel 2 - Working
+    [70 - 100)  - nivel 3 - Almost Done
+    100         - nivel 4 - Done
+ */
 public class Progress {
-    private String label;
-    private int value;
+    private static float PROGRESS_LIMITS[] = {0.2f, 0.4f, 0.7f, 1}; // folosit pt a determina niveleul
+    private static int PROGRESS_LEVEL[] = {0, 1, 2, 3, 4}; // folosit la afisari
+    private static String PROGRESS_LABEL[] = {"Just Started", "Something Done", "Working", "Almost Done", "Done"};// folosit la afisari
+    private static int FIRST_LEVEL  = 0;
+    private static int LAST_LEVEL   = 4;
+
+    private float value; // procent [0,1]
+    private int level; // [1..5]
+    private String label; // descriere pe intelesul tuturor
+
 
     public Progress() {
+        value = 0.0f;
+        updateProgress(value);
+    }
+
+    // C preia din V procentul PROC, iar newValue = PROC / 100
+    public void updateProgress(float newValue){
+        if (newValue < value) {
+            // exceptie [DEZV. ULTERIOARE]
+        }
+
+        value = newValue;
+        // in loc sa fac if () else if else if ....
+        level = calculateLevel(value);
+        // selecteaza level-ul corect in functie de procentul
+        // introdus de utilizator.
+
+        // actualizam si label-ul
+        label = PROGRESS_LABEL[level];
+        System.out.println(this);
+    }
+
+    // functie care stabileste nivelul, in functie de procentul introdus de utilizator
+    private int calculateLevel(float value) {
+        for (int level = FIRST_LEVEL; level < LAST_LEVEL; level++){
+            if (value < PROGRESS_LIMITS[level]) return level;
+        }
+        return LAST_LEVEL;
+    }
+
+    @Override
+    public String toString() {
+        return "value " + value
+                + ", level " + level
+                + ", " + label;
     }
 }
--- src/model/StandUpModel.java
+++ src/model/StandUpModel.java
@@ -5,8 +5,21 @@ import java.util.Timer;
 public class StandUpModel {
 
     public void test (){
-        Timer timer = new Timer();
-        TimerTask timerTask = new TimerTask();
-        timer.schedule(timerTask, 0, 1000);
+        //Timer timer = new Timer();
+        //TimerTask timerTask = new TimerTask();
+        //timer.schedule(timerTask, 0, 1000);
+
+        Progress progress = new Progress();
+        progress.updateProgress(0.19f);
+        progress.updateProgress(0.29f);
+        progress.updateProgress(0.39f);
+        progress.updateProgress(0.49f);
+        progress.updateProgress(0.59f);
+        progress.updateProgress(0.69f);
+        progress.updateProgress(0.79f);
+        progress.updateProgress(0.89f);
+        progress.updateProgress(0.99f);
+        progress.updateProgress(1.00f);
+
     }
 }
